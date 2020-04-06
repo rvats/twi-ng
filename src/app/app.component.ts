0f469470a61faf1ab0dceb30e771a7d7ac7386b7
Rahul Vats <vats.rahul@gmail.com>
2020-04-06 05:15:13 +0000

    RXJS Top 10 Operators Operator Number 10

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { timer, Observable, of } from 'rxjs';
+import { timer, Observable, of, from } from 'rxjs';
 import { Person } from './Person';
 
 @Component({
@@ -8,18 +8,36 @@ import { Person } from './Person';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
+
 export class AppComponent implements OnInit {
   public title: string = 'twi';
 
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
+
     const person: Person = {
       name: 'Rahul Vats',
     };
-    const source = timer(1000, 2000);
+
+    // of is used to create an observable from Data
+    // signature: of(...values, scheduler: Scheduler): Observable
+    // Emit variable amount of values in a sequence and then emits a complete notification.
     const personObs: Observable<Person> = of(person);
     personObs.subscribe(data=>console.log(data));
+
+    // from is used to create an observable from Promise
+    // signature: from(ish: ObservableInput, mapFn: function, thisArg: any, scheduler: Scheduler): Observable
+    // Turn an array, promise, or iterable into an observable.
+    // This operator can be used to convert a promise to an observable!
+    // For arrays and iterables, all contained values will be emitted as a sequence!
+    // This operator can also be used to emit a string as a sequence of characters!
+    const personPromise: Promise<Person> = Promise.resolve(person);
+    const personPromiseObs: Observable<Person> = from(personPromise);
+    personObs.subscribe(data=>console.log(data));
+
+    // This is misc code to learn more about RXJS operator timer
+    const source = timer(1000, 1000);
     const subscribe = source.subscribe(val => console.log(val));
     setTimeout(() => { subscribe.unsubscribe(); }, 10000);
   }
